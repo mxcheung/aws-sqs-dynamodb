4f33e35dfce259ac2278628219157e006a2a60b8
mxcheung <mxcheung@users.noreply.github.com>
2024-01-07 22:21:20 +1100

    Update sqs-cdk.ts

--- sqs-cdk/bin/sqs-cdk.ts
+++ sqs-cdk/bin/sqs-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { FortunesCdkStack } from '../lib/sqs-cdk-stack';
+import { SqsCdkStack } from '../lib/sqs-cdk-stack';
 
 const app = new cdk.App();
 new SqsCdkStack(app, 'SqsCdkStack', {
