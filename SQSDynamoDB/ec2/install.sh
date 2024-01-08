ab1387caeb52b31fa0fea377e5dceeb1a35124cb
mxcheung <mxcheung@users.noreply.github.com>
2024-01-08 17:25:35 +1100

    Update install.sh

--- SQSDynamoDB/ec2/install.sh
+++ SQSDynamoDB/ec2/install.sh
@@ -1,1 +1,8 @@
-install.sh
+#!/bin/bash
+# Creates fortune cookies via cdk
+
+python3 -m venv env
+source ./env/bin/activate
+pip install boto3
+pip install faker
+python3 send_message.py -q SqsCdkStack-Message251322C6-bWKhZTJ6oreC -i 0.1
